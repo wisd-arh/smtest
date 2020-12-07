f43c8b78bf484e5ba71751baa3e88ff35d13e3b5
Ivan Andreev <52032347+wisd-arh@users.noreply.github.com>
2020-12-07 18:59:43 +0300

    Message upload add

--- enj.php
+++ enj.php
@@ -1,42 +1,70 @@
 <?php
 include_once("pdo-debug.php");
 
-
 define("TASK_POST", "post");
+define("TASK_GET_ALL", "get");
 
-/*
-echo '<!DOCTYPE html>
-<html lang="ru">
-<head>
-<meta charset="UTF-8">
-</head>
-<body>
-' ;
-
-foreach ($_POST as $s) {
-  echo ($s);
-  '<br>';
-}
-try {
-  $db = new PDO('mysql:host=localhost;dbname=smtest', 'root', '');
-} catch (PDOException $e) {
-  print "Error!: " . $e->getMessage();
-  die();
-}
-
-$stmt = $db->query("SELECT * FROM messages");
-while ($row = $stmt->fetch())
+class tTask
 {
-  echo '<pre>';
-  print_r($row);
+  private $taskClass;
+
+  public function __construct($taskClass)
+  {
+    $this->taskClass = $taskClass;
+  }
+
+  private function postMessage()
+  {
+    $db = new PDO('mysql:host=localhost;dbname=smtest;charset=UTF8;', 'root', '');
+
+    $mess = new tMessage(new DateTime(), $_POST["user"], $_POST["email"], $_POST["message"]);
+
+    $parameters = array(
+      'p1' => $mess->time->format('Y-m-d H:i:s'),
+      'p2' => $mess->user,
+      'p3' => $mess->email,
+      'p4' => $mess->message
+    );
+
+    $sql = "INSERT INTO `messages` (`time`, `user`, `email`, `message`) VALUES (:p1, :p2, :p3, :p4)";
+    $stmt = $db->prepare($sql);
+    try {
+      $stmt->execute($parameters);
+    } catch (PDOException $e) {
+      echo 'Execute не удалось: ' . $e->getMessage();
+    }
+    if ($stmt->rowCount() == 1) {
+      echo 'SUCCESS';
+    }
+    $stmt = null;
+    $db = null;
+  }
+
+  private function getAllMessages() {
+    $db = new PDO('mysql:host=localhost;dbname=smtest;charset=UTF8;', 'root', '');
+    $sql = "SELECT * FROM `messages`";
+    $sth = $db->query($sql);
+
+    $results = $sth->fetchAll(PDO::FETCH_ASSOC);
+    $json = json_encode($results);
+    echo $json;
+  }
+
+  public function execute()
+  {
+    switch ($this->taskClass) {
+      case TASK_POST:
+        $this->postMessage();
+        break;
+      case TASK_GET_ALL:
+        $this->getAllMessages();
+        break;
+      case 2:
+        echo "i равно 2";
+        break;
+    }
+  }
 }
-
-echo '
-</body>
-</html>
-
-';
-*/
 
 class tMessage
 {
@@ -68,33 +96,17 @@ class tMessage
   }
 }
 
-date_default_timezone_set('Europe/Moscow');
 
-$db = new PDO('mysql:host=localhost;dbname=smtest;charset=UTF8;', 'root', ''); //time_zone="Europe/Moscow";
+$task = new tTask($_POST["task"]);
+$task->execute();
+$task = null;
 
-$mess = new tMessage(new DateTime(), $_POST["user"], $_POST["email"], $_POST["message"]);
 
-$parameters = array(
-  'p1' => $mess->time->format('Y-m-d H:i:s'),
-  'p2' => $mess->user,
-  'p3' => $mess->email,
-  'p4' => $mess->message
-);
 
-$sql = "INSERT INTO `messages` (`time`, `user`, `email`, `message`) VALUES (:p1, :p2, :p3, :p4)";
-$stmt = $db->prepare($sql);
-try {
-  $stmt->execute($parameters);
-} catch (PDOException $e) {
-  echo 'Execute не удалось: ' . $e->getMessage();
-}
-if ( $stmt->rowCount() == 1) {
-  echo 'SUCCESS';
-
-}
 
 /*
-
+date_default_timezone_set('Europe/Moscow');
+//time_zone="Europe/Moscow";
 /*
 $task = $_POST["task"];
 
@@ -174,4 +186,36 @@ $params = [
 $stmt = $db->prepare($query);
 $stmt->execute($params);
 */
-?>
+/*
+echo '<!DOCTYPE html>
+<html lang="ru">
+<head>
+<meta charset="UTF-8">
+</head>
+<body>
+' ;
+
+foreach ($_POST as $s) {
+  echo ($s);
+  '<br>';
+}
+try {
+  $db = new PDO('mysql:host=localhost;dbname=smtest', 'root', '');
+} catch (PDOException $e) {
+  print "Error!: " . $e->getMessage();
+  die();
+}
+
+$stmt = $db->query("SELECT * FROM messages");
+while ($row = $stmt->fetch())
+{
+  echo '<pre>';
+  print_r($row);
+}
+
+echo '
+</body>
+</html>
+
+';
+*/
--- site_scripts.js
+++ site_scripts.js
@@ -13,7 +13,7 @@ function getXmlHttp() {
     if (!xmlHttp && typeof XMLHttpRequest != 'undefined') {
         xmlHttp = new XMLHttpRequest();
     }
-    
+
     return xmlHttp;
 }
 
@@ -26,12 +26,60 @@ function updatePage() {
                 status.innerHTML = 'успешно отправлено';
                 status.classList.toggle('status_hide');
                 status.classList.toggle('status_show');
-                setTimeout(function(){
+                setTimeout(function() {
                     status.classList.toggle('status_hide');
                 }, 2000);
+                addMessage();
             } else {
                 document.getElementById("messages").innerHTML = response;
-            }    
+            }
+        }
+    }
+}
+
+function addMessage() {
+    let message_list = document.getElementById("message_list");
+
+    let newMessage = document.createElement("li");
+    var now = new Date();
+    newMessage.textContent = now;
+
+    message_list.insertBefore(newMessage, message_list.firstChild);
+
+}
+
+function updateMessages() {
+    if (xmlhttp.readyState == 4) {
+        if (xmlhttp.status == 200) {
+            let message_list = document.getElementById("message_list");
+            let response = JSON.parse(xmlhttp.responseText);
+
+            response.forEach(function(item, i, response) {
+                let li = document.createElement("li");
+                let d = document.createElement("div");
+                d.classList.add("container");
+                d.classList.add("clearfix");
+                let t = document.createElement("div");
+                t.classList.add("time");
+                t.textContent = item.time;
+                d.appendChild(t);
+                let e = document.createElement("div");
+                e.classList.add("email");
+                e.textContent = item.email;
+                d.appendChild(e);
+                let u = document.createElement("div");
+                u.classList.add("user");
+                u.textContent = item.user;
+                d.appendChild(u);
+                let m = document.createElement("div");
+                m.classList.add("message");
+                m.textContent = item.message;
+                d.appendChild(m);
+                li.appendChild(d);
+                message_list.insertBefore(li, message_list.firstChild);
+            });
+
+            //                d.textContent = res[0].idmessages; console.log(res); console.log(res[0]); console.log(res[0].idmessages); document.getElementById("messages").appendChild(d);
         }
     }
 }
